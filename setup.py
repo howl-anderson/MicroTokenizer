20b738aec3f3724d923f7a16a07289c023d34add
Xiaoquan Kong <u1mail2me@gmail.com>
2018-07-02 22:26:59 +0800

    Add dependency

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', 'networkx>=2.1']
+requirements = ['Click>=6.0', 'networkx>=2.1', 'tqdm']
 
 setup_requirements = ['pytest-runner', ]
 
