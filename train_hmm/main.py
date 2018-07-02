489d42c7bad094ba7b93058c7ad732232c482495
Xiaoquan Kong <u1mail2me@gmail.com>
2018-07-02 22:26:36 +0800

    Add script for training HMM tokenizer

---
+++ train_hmm/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from train_hmm import tool
+from train_hmm.train import HMMTagger
+
+hmm_tagger = HMMTagger()
+
+
+# tool.driver(19000, 100, hmm_tagger.train_one_line, hmm_tagger.predict)
+tool.driver(290000, 1, hmm_tagger.train_one_line, hmm_tagger.predict)
+result = hmm_tagger.predict("王小明在北京的清华大学读书。")
+print(result)
---
+++ train_hmm/train.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from MicroHMM.hmm import HMMModel
+
+
+class HMMTagger(object):
+    def __init__(self):
+        self.hmm_model = HMMModel()
+
+    def train_one_line(self, line):
+        line = line.strip()
+
+        list_of_word_tag_pair = []
+        for word in line.split():
+            word = word.strip()
+
+            tag = self._generate_char_tag_for_word(word)
+
+            list_of_word_tag_pair.extend(
+                list(zip(word, tag))
+            )
+
+        self.hmm_model.train_one_line(list_of_word_tag_pair)
+
+    @staticmethod
+    def _generate_char_tag_for_word(word):
+        len_of_word = len(word)
+
+        if len_of_word == 1:
+            return 'S'
+
+        if len_of_word >= 2:
+            number_of_middle = len_of_word - 2
+            return 'B' + 'M' * number_of_middle + 'E'
+
+    def predict(self, line, output_graphml_file=None):
+        char_list = line
+
+        char_tag_pair = self.hmm_model.predict(char_list, output_graphml_file)
+
+        token_list = []
+        word_char = []
+        for char, tag in char_tag_pair:
+            # no matter what, word_char still need record
+            word_char.append(char)
+
+            if tag == "S" or tag == "E":
+                # emission token word
+                word = "".join(word_char)
+                token_list.append(word)
+
+                # reset word_char cache
+                word_char = []
+
+        return token_list
+
+
+if __name__ == "__main__":
+    hmm_tagger = HMMTagger()
+    hmm_tagger.train_one_line("我/A 是/B 中国人/C")
+    hmm_tagger.train_one_line("你/A 打/B 人/C")
+    result = hmm_tagger.predict("你 打 人")
+    print(result)
