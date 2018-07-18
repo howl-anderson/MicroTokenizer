e2cc3471174893241e895961e1f1d1bf9efbfe0b
Xiaoquan Kong <u1mail2me@gmail.com>
2018-07-18 23:01:12 +0800

    Clean up code: remove redundant codes

--- MicroTokenizer/__init__.py
+++ MicroTokenizer/__init__.py
@@ -12,41 +12,8 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 default_dict_file = os.path.join(current_dir, 'dictionary', 'dict.txt')
 
-
-def read_dict(dict_file):
-    dict_data = {}
-
-    with open(dict_file, encoding='utf_8') as fd:
-        for line in fd:
-            splited_line = line.split(' ')
-            data = dict(enumerate(splited_line))
-
-            token = data[0]
-            frequency = data[1].strip()  # using strip to clean tailing newline symbol
-            part_of_speech = data.get(2, '').strip()  # using strip to clean tailing newline symbol
-
-            # print(token, frequency)
-
-            dict_data[token] = int(frequency)
-
-    return dict_data
-
-
-def compute_edge_weight(dict_data):
-    # get total weight count for compute possibility
-    total_weight = sum(dict_data.values())
-
-    # recompute the weight
-    for k, v in dict_data.items():
-        # possibility = count / total_count
-        # reciprocal of possibility can turn max value to min value, which can be
-        # used for search max value path by search shortest path
-        # log function can turn multiplication to addition: log(A * B) = log(A) + log(B)
-        dict_data[k] = math.log(total_weight / v)
-
-
-dict_data = read_dict(default_dict_file)
-compute_edge_weight(dict_data)
+# global cache for dict_data
+dict_data = None
 
 
 def cut(message, HMM=False):
@@ -59,6 +26,12 @@ def cut(message, HMM=False):
 def cut_by_DAG(message):
     # NOTE: this import statement can not put the head line for it will cause cycle import
     from MicroTokenizer.MicroTokenizer import MicroTokenizer
+
+    global dict_data
+
+    if dict_data is None:
+        dict_data = MicroTokenizer.read_dict(default_dict_file)
+        MicroTokenizer.compute_edge_weight(dict_data)
 
     micro_tokenizer = MicroTokenizer(dict_data)
     micro_tokenizer.build_graph(message)
