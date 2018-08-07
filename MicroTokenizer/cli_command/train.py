c70f6028a9f1bc48eee59f803e09cfaf33ee26b0
Xiaoquan Kong <u1mail2me@gmail.com>
2018-08-07 15:07:23 +0800

    Bugfix in training interface definition

--- MicroTokenizer/cli_command/train.py
+++ MicroTokenizer/cli_command/train.py
@@ -6,7 +6,7 @@ from ..train.train import train as train_func
 @plac.annotations(
     output_dir=("output directory to store model in", "positional", None, str),
     train_data=("location of JSON-formatted training data", "positional",
-                None, str)
+                None, str, None, "n")
 )
-def train(output_dir, train_data):
+def train(output_dir, *train_data):
     train_func(train_data, output_dir)
