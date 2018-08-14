dd9b6aa91d7e0e270218ce14e495558e41a734aa
Xiaoquan Kong <u1mail2me@gmail.com>
2018-08-14 14:38:45 +0800

    Update version

--- MicroTokenizer/__init__.py
+++ MicroTokenizer/__init__.py
@@ -5,7 +5,7 @@ import os
 
 __author__ = """Xiaoquan Kong"""
 __email__ = 'u1mail2me@gmail.com'
-__version__ = '0.1.6'
+__version__ = '0.1.6.1'
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
