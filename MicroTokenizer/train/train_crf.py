84612bf9aff86fceb8a380923088c837d8b0dc05
Xiaoquan Kong <u1mail2me@gmail.com>
2018-08-18 18:15:56 +0800

    Fix: should using model_dir instead of model_file

--- MicroTokenizer/train/train_crf.py
+++ MicroTokenizer/train/train_crf.py
@@ -8,17 +8,19 @@ from tqdm import tqdm
 def train_crf(input_files_list, output_dir):
     crf_trainer = CRFTrainer()
 
+    output_file = os.path.join(output_dir, 'model.crfsuite')
+
     for data_file in input_files_list:
         with open(data_file) as fd:
             for line in tqdm(fd):
                 crf_trainer.train_one_raw_line(line)
 
-    crf_trainer.train(output_dir)
+    crf_trainer.train(output_file)
 
 
 if __name__ == "__main__":
     current_dir = os.path.dirname(os.path.abspath(__file__))
     train_crf(
         [os.path.join(current_dir, "./data.txt")],
-        os.path.join(current_dir, "./model.crfsuite")
+        os.path.join(current_dir, "./")
     )
