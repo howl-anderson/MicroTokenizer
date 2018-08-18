3543f92f83888b646486d47832015210203940b6
Xiaoquan Kong <u1mail2me@gmail.com>
2018-08-18 18:17:09 +0800

    Add CRF model training CLI

--- MicroTokenizer/train/train.py
+++ MicroTokenizer/train/train.py
@@ -1,7 +1,9 @@
 from .train_hmm import train_hmm
 from .train_dag import train_dag
+from .train_crf import train_crf
 
 
 def train(input_files_list, output_dir):
     train_dag(input_files_list, output_dir)
     train_hmm(input_files_list, output_dir)
+    train_crf(input_files_list, output_dir)
