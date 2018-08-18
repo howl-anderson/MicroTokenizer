764724c6fd230f915951e003f764bd0d149b53ce
Xiaoquan Kong <u1mail2me@gmail.com>
2018-08-19 00:16:06 +0800

    Add long_description_content_type='text/markdown' to setup.py

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='MicroTokenizer',
     name='MicroTokenizer',
