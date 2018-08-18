f3d9c33dae44d6f9e49e72f0aab7656bde1dd08a
Xiaoquan Kong <u1mail2me@gmail.com>
2018-08-18 18:12:40 +0800

    Fix function name

--- MicroTokenizer/train/train_crf.py
+++ MicroTokenizer/train/train_crf.py
@@ -5,7 +5,7 @@ from MicroTokenizer.CRF.crf_trainer import CRFTrainer
 from tqdm import tqdm
 
 
-def train_hmm(input_files_list, output_dir):
+def train_crf(input_files_list, output_dir):
     crf_trainer = CRFTrainer()
 
     for data_file in input_files_list:
@@ -18,7 +18,7 @@ def train_hmm(input_files_list, output_dir):
 
 if __name__ == "__main__":
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    train_hmm(
+    train_crf(
         [os.path.join(current_dir, "./data.txt")],
         os.path.join(current_dir, "./model.crfsuite")
     )
