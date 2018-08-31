706d4e44668c6ead4d0149b9b4d7050eb9034825
Xiaoquan Kong <u1mail2me@gmail.com>
2018-08-31 13:59:08 +0800

    Bugfix: add python2 compatible

--- MicroTokenizer/DAG/dictionary/trie_algorithm.py
+++ MicroTokenizer/DAG/dictionary/trie_algorithm.py
@@ -3,7 +3,7 @@ from MicroTokenizer.DAG.dictionary.dictionary import DictionaryData
 
 class TreeNode(dict):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super(TreeNode).__init__(self, *args, **kwargs)
 
         self.is_leaf = False
         self.weight = None
@@ -15,7 +15,7 @@ class TreeNode(dict):
 
 class TrieAlgorithm(DictionaryData):
     def __init__(self, *args, reverse=False, **kwargs):
-        super().__init__(*args, **kwargs)
+        super(TrieAlgorithm).__init__(self, *args, **kwargs)
 
         # reverse the char order from head to tail
         self.reverse = reverse
