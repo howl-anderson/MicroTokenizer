ffb7154abf597d5873cc708a603e3b9fc48c0b20
Xiaoquan Kong <u1mail2me@gmail.com>
2018-08-31 15:03:01 +0800

    Bugfix: python 2 compatible

--- MicroTokenizer/dag.py
+++ MicroTokenizer/dag.py
@@ -11,7 +11,7 @@ from MicroTokenizer.DAG.dictionary.train_dictionary import TrainDictionary
 
 class DAGTokenizer(BaseTokenizer):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super(DAGTokenizer, self).__init__(*args, **kwargs)
 
         self.graph_builder = None  # type: GraphBuilder
         self.train_dictionary = TrainDictionary()
@@ -24,7 +24,9 @@ class DAGTokenizer(BaseTokenizer):
 
         self.graph_builder = NonRecursiveAlgorithm(self.dict_data)
 
-    def segment(self, message: str):
+    def segment(self, message):
+        # type: (str) -> List[str]
+
         self.graph_builder.init_graph()
         self.graph_builder.build_graph(message)
 
@@ -35,7 +37,9 @@ class DAGTokenizer(BaseTokenizer):
         # remove start and end token
         return raw_token[1:-1]
 
-    def train_one_line(self, token_list: List[str]):
+    def train_one_line(self, token_list):
+        # type: (List[str]) -> None
+
         self.train_dictionary.train_one_line(token_list)
 
     def do_train(self):
@@ -46,5 +50,7 @@ class DAGTokenizer(BaseTokenizer):
 
         self.graph_builder = NonRecursiveAlgorithm(self.dict_data)
 
-    def persist_to_dir(self, output_dir: str):
+    def persist_to_dir(self, output_dir):
+        # type: (str) -> None
+
         self.train_dictionary.persist_to_dir(output_dir)
