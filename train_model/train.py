dd662d2973b573b7bd0bbe4088e9dae52cc6f658
Xiaoquan Kong <u1mail2me@gmail.com>
2018-08-31 23:39:16 +0800

    Bugfix: output argument is incorrect

--- train_model/train.py
+++ train_model/train.py
@@ -6,6 +6,6 @@ from MicroTokenizer.train.train import train
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 input_file_list = [os.path.join(current_dir, 'data.txt')]
-output_dir = [os.path.join(current_dir, '../MicroTokenizer/model_data')]
+output_dir = os.path.join(current_dir, '../MicroTokenizer/model_data')
 
 train(input_file_list, output_dir)
