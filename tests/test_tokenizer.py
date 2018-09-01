d4c285b218879da459ffe59f34e5d2aa9f416e33
Xiaoquan Kong <u1mail2me@gmail.com>
2018-09-01 09:33:14 +0800

    Bugfix in test case

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -10,7 +10,7 @@ tokenizer = Tokenizer()
 def test_DAG():
     input_text = "你打人"
 
-    result = tokenizer.cut_by_DAG("他走了")
+    result = tokenizer.cut_by_DAG(input_text)
 
     pytest.helpers.assert_token_equals(result, input_text)
 
@@ -18,7 +18,7 @@ def test_DAG():
 def test_HMM():
     input_text = "你打人"
 
-    result = tokenizer.cut_by_HMM("他走了")
+    result = tokenizer.cut_by_HMM(input_text)
 
     pytest.helpers.assert_token_equals(result, input_text)
 
@@ -26,7 +26,7 @@ def test_HMM():
 def test_CRF():
     input_text = "你打人"
 
-    result = tokenizer.cut_by_CRF("他走了")
+    result = tokenizer.cut_by_CRF(input_text)
 
     pytest.helpers.assert_token_equals(result, input_text)
 
@@ -34,7 +34,7 @@ def test_CRF():
 def test_max_match_forward():
     input_text = "你打人"
 
-    result = tokenizer.cut_by_max_match_forward("他走了")
+    result = tokenizer.cut_by_max_match_forward(input_text)
 
     pytest.helpers.assert_token_equals(result, input_text)
 
@@ -42,7 +42,7 @@ def test_max_match_forward():
 def test_max_match_backward():
     input_text = "你打人"
 
-    result = tokenizer.cut_by_max_match_backward("他走了")
+    result = tokenizer.cut_by_max_match_backward(input_text)
 
     pytest.helpers.assert_token_equals(result, input_text)
 
@@ -50,6 +50,6 @@ def test_max_match_backward():
 def test_max_match_bidirectional():
     input_text = "你打人"
 
-    result = tokenizer.cut_by_max_match_bidirectional("他走了")
+    result = tokenizer.cut_by_max_match_bidirectional(input_text)
 
     pytest.helpers.assert_token_equals(result, input_text)
