fc8ac8f474004bd86338cc09e81d415ca468357d
Xiaoquan Kong <u1mail2me@gmail.com>
2018-09-21 00:36:28 +0800

    Bugfix: train parallel is not supported

--- train_model/train.py
+++ train_model/train.py
@@ -8,7 +8,4 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 input_file_list = [os.path.join(current_dir, 'data.txt')]
 output_dir = os.path.join(current_dir, '../MicroTokenizer/model_data')
 
-if os.environ.get('DEBUG_MICRO_TOKENIZER', False):
-    train(input_file_list, output_dir)
-else:
-    train_parallel(input_file_list, output_dir)
+train(input_file_list, output_dir)
