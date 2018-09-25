047e6a05ebd75909aabdf67de9425d33dc31fefe
Xiaoquan Kong <u1mail2me@gmail.com>
2018-09-25 14:55:24 +0800

    Add a TODO

--- MicroTokenizer/cli_command/train.py
+++ MicroTokenizer/cli_command/train.py
@@ -4,6 +4,7 @@ from MicroTokenizer.train.train import train as train_func
 
 
 # TODO: plac based CLI may have bug, need be checked
+# TODO: Add configure file option for CRF feature function list
 @plac.annotations(
     output_dir=("output directory to store model in", "positional", None, str),
     train_data=("location of training data", "positional",
