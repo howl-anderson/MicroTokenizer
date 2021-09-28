350b168cb58fdc7d1e5fb97459b5f5868a8aaaa0
Xiaoquan Kong <u1mail2me@gmail.com>
2021-09-28 16:45:16 +0800

    Bump version: 0.20.4 → 0.21.0

--- MicroTokenizer/__init__.py
+++ MicroTokenizer/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for Micro Tokenizer for Chinese."""
-__version__ = "0.20.4"
+__version__ = "0.21.0"
 
 import os
 
--- setup.py
+++ setup.py
@@ -83,6 +83,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/howl-anderson/MicroTokenizer',
-    version="0.20.4",
+    version="0.21.0",
     zip_safe=False,
 )
